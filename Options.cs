af3822daea5f8d6ab97dd915aa640455339feb8f
Shailen  Sukul <shailensukul@gmail.com>
2020-01-13 22:05:54 +1100

    added commandlineparger nuget

---
+++ Options.cs
@@ -0,0 +1,31 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sukul.Media.Backup
+{
+    public class Options
+    {
+        [Option('s', "source", Required = true, HelpText = "Source path to folder containing files.")]
+        public string SourcePath { get; set; }
+
+        [Option('d', "destination", Required = true, HelpText = "Destination path to folder containing files.")]
+        public string DestinationPath { get; set; }
+
+        [Option(
+            'i',
+            "image",
+          Default = true,
+          HelpText = "Copy all types of image files.")]
+        public bool Images { get; set; }
+
+        [Option(
+          'v', 
+          "video",
+          Default = false,
+          HelpText = "Copy all types of video files.")]
+        public bool Videos { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,24 +1,39 @@
 ﻿namespace Sukul.Media.Backup
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.Reflection;
+    using CommandLine;
     using SixLabors.ImageSharp.Metadata.Profiles.Exif;
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0)
-            // https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
-            {
-                var tags = ImageHelper.EXIFData(File.ReadAllBytes($"{args[0]}"));
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+               .WithParsed(RunOptions)
+               .WithNotParsed(HandleParseError);
 
-                foreach (var tag in tags)
-                {
-                    Console.WriteLine($"Tag: {tag.Key} | Value: {tag.Value}");
-                }
-                Console.ReadLine();
-            }
+            //if (args.Length > 0)
+            //// https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
+            //{
+            //    var tags = ImageHelper.EXIFData(File.ReadAllBytes($"{args[0]}"));
+
+            //    foreach (var tag in tags)
+            //    {
+            //        Console.WriteLine($"Tag: {tag.Key} | Value: {tag.Value}");
+            //    }
+            //    Console.ReadLine();
+            //}
+        }
+
+        static void RunOptions(Options opts)
+        {
+            //handle options
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+            //handle errors
         }
     }
 }
