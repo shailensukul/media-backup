bca709af4036be1494e1303ca69a905666236286
Shailen  Sukul <shailensukul@gmail.com>
2020-06-26 21:58:09 +1000

    Added tracing

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿namespace Sukul.Media.Backup
+﻿// Define the TRACE directive, which enables trace output to the
+// Trace.Listeners collection. Typically, this directive is defined
+// as a compilation argument.
+#define TRACE
+namespace Sukul.Media.Backup
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.IO;
     using System.Reflection;
     using CommandLine;
     using SixLabors.ImageSharp.Metadata.Profiles.Exif;
+
     class Program
     {
+
         static void Main(string[] args)
         {
+            Trace.Listeners.Add(new ConsoleTraceListener(true));
             CommandLine.Parser.Default.ParseArguments<Options>(args)
                .WithParsed(RunOptions)
                .WithNotParsed(HandleParseError);
@@ -30,10 +38,21 @@
         static void RunOptions(Options opts)
         {
             //handle options
+            Trace.Indent();
+            Trace.WriteLine($"Source: {opts.SourcePath}");
+            Trace.WriteLine($"Destination: {opts.DestinationPath}");
+            Trace.WriteLine($"Copy images: {opts.Images}");
+            Trace.WriteLine($"Copy videos: {opts.Videos}");
+
+            Console.ReadLine();
         }
         static void HandleParseError(IEnumerable<Error> errs)
         {
             //handle errors
+            foreach (var err in errs)
+            {
+                Console.WriteLine(err.ToString());
+            }
         }
     }
 }
