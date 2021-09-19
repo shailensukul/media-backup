7d2bda33a5c4cbb1ffb9845c0349c2b7c4214646
Shailen Sukul <shailensukul@gmail.com>
2021-09-19 22:22:51 +1000

    Refactoring creation date discovery and format string .. WIP

--- media_backup.shared/Coordinator.cs
+++ media_backup.shared/Coordinator.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MetadataExtractor;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -21,6 +23,12 @@ namespace Sukul.Media.Backup.Shared
 
         private readonly ICollection<Task> _tasks;
 
+        private static Collection<MediaCreatedDateTimeMapping> CreatedDateTimeMappings = new Collection<MediaCreatedDateTimeMapping>()
+        {
+            new MediaCreatedDateTimeMapping { DirectoryName="QuickTime Movie Header", Name="Created", DateTimeParseFormat = "ddd MMM dd hh:mm:ss yyyy" },
+            new MediaCreatedDateTimeMapping { DirectoryName="Exif IFD0", Name="Date/Time", DateTimeParseFormat = "yyyy:MM:dd HH:mm:ss" }
+        };
+
         public Coordinator(S source, D destination)
         {
             _source = source;
@@ -28,7 +36,7 @@ namespace Sukul.Media.Backup.Shared
             _tasks = new Collection<Task>();
         }
 
-        public async void ProcessAsync(string sourcePath, string destinationPath, bool recursive, bool processImages, bool processVideos, 
+        public async void ProcessAsync(string sourcePath, string destinationPath, bool recursive, bool processImages, bool processVideos,
             CancellationToken cancellation)
         {
             try
@@ -54,59 +62,68 @@ namespace Sukul.Media.Backup.Shared
             }
         }
 
+        private static DateTime ParseDateTime(string mediaDateTime, DateTime fr)
+        {
+            DateTime dateTime = default(DateTime);
+
+            if (DateTime.TryParseExact(Convert.ToString(mediaDateTime), "ddd MMM dd hh:mm:ss yyyy", CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTime))
+            {
+            }
+            else if (DateTime.TryParseExact(Convert.ToString(mediaDateTime), "yyyy: MM:dd HH: mm:ss", CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTime))
+            {
+            }
+
+            return dateTime;
+        }
+
         public static async Task CopyItemToDestination(SourceMedia media, string destinationPath, S source, D destination, CancellationToken cancellation)
         {
             DateTime dateTime = default(DateTime);
-            var tags = ImageHelper.EXIFData(media.Data);
-            object date;
+
+            IEnumerable<MetadataExtractor.Tag> tags;
+            using (var stream = new MemoryStream(media.Data))
+            {
+                tags = ImageMetadataReader.ReadMetadata(stream).SelectMany(d => d.Tags);
+            }
+
             string desinationFolder;
 
-            if (tags != null && tags.TryGetValue("ModifyDate", out date))
+            var mappings = from m in CreatedDateTimeMappings
+                           join t in tags
+                           on new { m.DirectoryName, m.Name } equals new { t.DirectoryName, t.Name }
+                           select (Mapping: m, Tag: t);
+
+            if (!mappings.Any())
             {
-                if (DateTime.TryParseExact(Convert.ToString(date), "yyyy:MM:dd HH:mm:ss",
-                CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTime))
-                {
-                    {
-                        desinationFolder = $"{destinationPath}\\{dateTime.Year}\\{dateTime.Month.ToString().PadLeft(2, '0')}\\{dateTime.Day.ToString().PadLeft(2, '0')}";
-                    }
-                }
+                Trace.WriteLine($"Unable to determine creation date for {media} Skipping .. ");
             }
             else
             {
-                if (tags != null && tags.TryGetValue("DateTime", out date))
+                var mapping = mappings.First();
+                if (DateTime.TryParseExact(Convert.ToString(mapping.Tag.Description), mapping.Mapping.DateTimeParseFormat, CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTime))
                 {
-                    if (DateTime.TryParseExact(Convert.ToString(date), "yyyy:MM:dd HH:mm:ss",
-                    CultureInfo.CurrentCulture, DateTimeStyles.None, out dateTime))
+                    desinationFolder = $"{destinationPath}\\{dateTime.Year}\\{dateTime.Month.ToString().PadLeft(2, '0')}\\{dateTime.Day.ToString().PadLeft(2, '0')}";
+
+                    //desinationFolder = $"{destinationPath}\\{dateTime.Year}\\{dateTime.Month.ToString().PadLeft(2, '0')}\\{dateTime.Day.ToString().PadLeft(2, '0')}";
+
+                    cancellation.ThrowIfCancellationRequested();
+
+                    Trace.WriteLine($"Copying file to  {desinationFolder}");
+                    await destination.SaveAsync(desinationFolder, media.Data, media.Extension);
+                    try
                     {
-                        {
-                            desinationFolder = $"{destinationPath}\\{dateTime.Year}\\{dateTime.Month.ToString().PadLeft(2, '0')}\\{dateTime.Day.ToString().PadLeft(2, '0')}";
-                        }
+                        cancellation.ThrowIfCancellationRequested();
+                        source.Delete(media);
+                    }
+                    catch
+                    {
+                        Trace.WriteLine($"Unable to delete {media}. Please remove manually.");
                     }
                 }
-            }
-
-            if (dateTime == default(DateTime))
-            {
-                dateTime = media.CreationDateTime;
-            }
-
-            desinationFolder = $"{destinationPath}\\{dateTime.Year}\\{dateTime.Month.ToString().PadLeft(2, '0')}\\{dateTime.Day.ToString().PadLeft(2, '0')}";
-
-            cancellation.ThrowIfCancellationRequested();
-
-            // Check whether the file already exists in destination folder
-
-
-            Trace.WriteLine($"Copying file to  {desinationFolder}");
-            await destination.SaveAsync(desinationFolder, media.Data, media.Extension);
-            try
-            {
-                cancellation.ThrowIfCancellationRequested();
-                source.Delete(media);
-            }
-            catch (Exception ex)
-            {
-                Trace.WriteLine($"Unable to delete {media}. Please remove manually.");
+                else
+                {
+                    Trace.WriteLine($"Unable to parse creation date ({mapping.Tag.Description}) with format {mapping.Mapping.DateTimeParseFormat} for {media} Skipping .. ");
+                }
             }
         }
     }
---
+++ media_backup.shared/MediaCreatedDateTimeMapping.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sukul.Media.Backup.Shared
+{
+    /// <summary>
+    /// Maps the Created datetime to the format it is saved in
+    /// </summary>
+    public sealed class MediaCreatedDateTimeMapping
+    {
+        public string DirectoryName { get; set; }
+        public string Name { get; set; }
+        public string DateTimeParseFormat { get; set; }
+    }
+}
